580a884651de0d4a930006c283f3f80ed4173f27
ThanhDang3105 <thanhls1235ls12@gmail.com>
2022-07-27 17:04:09 +0700

    update

--- src/index.js
+++ src/index.js
@@ -8,9 +8,11 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <Provider store={store}>
-        <App />
-    </Provider>,
+    <StrictMode>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
